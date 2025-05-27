d2c07584a954e632e01cee13fbc99058683e256a
Simon Andersen <smpa20@student.aau.dk>
2025-05-27 15:16:57 +0200

    added more vizualisation

--- src/app.py
+++ src/app.py
@@ -376,7 +376,7 @@ def main():
     # microgrid.reset()
 
     wait_time = (
-        300  # 120 seconds to make the simulation 30x times faster than real time.
+        120  # 120 seconds to make the simulation 30x times faster than real time.
     )
     starttime = time.monotonic()
 
